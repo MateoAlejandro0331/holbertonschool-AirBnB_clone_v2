d598ce42d2b9b518456d69c3c5c530f433588758
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-05 18:02:38 -0500

    adding task 3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- MySQL setup test - prepares a MySQL server for test the project
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test .* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
