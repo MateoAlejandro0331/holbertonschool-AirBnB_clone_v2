d0528204b218f084aeb6d0b0700307150a5d2d8f
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-06 11:55:14 -0500

    adding changes to diferente files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+Base = declarative_base()
 
-
-class State(BaseModel):
+class State(BaseModel(Base)):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    
