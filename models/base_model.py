f218de053a70ea6507c75483e54225f57e562a5c
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-06 11:39:12 -0500

    update base_model and city, for the first to points of task 6

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from lib2to3.pgen2.token import STRING
+from sqlalchemy import Column, Integer, String, Time
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
 
 
+Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), autoincrement=True,
+                nullable=False, primary_key=True)
+    created_at = Column(Time(), nullable=False)
+    updated_at = Column(Time(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -41,4 +50,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary['_sa_instance_state']:
+            del dictionary['_sa_instance_state']
         return dictionary
+    
+    def delete(self):
+        from models import storage
+        storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+Base = declarative_base()
 
 
-class City(BaseModel):
+class City(BaseModel(Base)):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False, ForeignKey("states.id"))
