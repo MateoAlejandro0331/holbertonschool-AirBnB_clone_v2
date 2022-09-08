14178ef184aa8aa326e6400d6b79937de89635d5
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-08 16:56:51 -0500

    adding task 6

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -7,5 +8,9 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -8,5 +9,8 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)    
-    cities = relationship('City', cascade='all, delete, delete-orphan')
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)    
+    # cities = relationship('City', cascade='all, delete, delete-orphan')
+    else:
+        name = ""
