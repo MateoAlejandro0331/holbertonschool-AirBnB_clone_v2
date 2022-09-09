4a666cfc55a2184d09165102af2589f4312f1bed
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-09 15:13:55 -0500

    Fixed name atribute

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(60), nullable=False)
+        name = Column(String(128), nullable=False)
         description = Column(String(1024))
         number_rooms = Column(Integer(), nullable=False, default=0)
         number_bathrooms = Column(Integer(), nullable=False, default=0)
