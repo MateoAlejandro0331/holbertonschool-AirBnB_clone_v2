7732b72ee54f473b8e75049f259263fb20b1c16b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-09 11:07:36 -0500

    adding state file with cities relationship

--- models/state.py
+++ models/state.py
@@ -11,6 +11,6 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         name = Column(String(128), nullable=False)
-    # cities = relationship('City', cascade='all, delete', backref='state')
+        cities = relationship('City', cascade='all, delete', backref='state')
     else:
         name = ""
