d7bde147173a3e64eaeb8c8ea21ac47ac63156f8
AFOP <4592@holbertonschool.com>
2022-09-09 21:08:49 -0500

    Update Task 9

--- models/review.py
+++ models/review.py
@@ -11,7 +11,7 @@ class Review(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
         place_id = ""
         user_id = ""
