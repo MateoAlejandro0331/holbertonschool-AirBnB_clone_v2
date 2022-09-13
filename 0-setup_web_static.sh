adeeba3ac7e998ce3dc31c965b2f55f06c36e34a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-12 23:56:07 -0500

    adding task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -27,6 +27,6 @@ sudo touch /data/web_static/releases/test/index.html
 sudo chmod 777 /data/web_static/releases/test/index.html
 echo $print > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/listen 80 default_server/ a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo service nginx restart
