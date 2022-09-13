5f41c483c9e4b7dae4dcd3cb8542725118985e40
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-12 23:51:31 -0500

    adding task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+if ! command -v nginx -v &> /dev/null
+then
+    sudo apt-get update
+    sudo apt -y install nginx
+fi
+print="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
+sudo chmod 777 /data/web_static/releases/test/index.html
+echo $print > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown ubuntu:ubuntu /data/
+sudo sed -i "/listen 80 default_server/ a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo service nginx restart
