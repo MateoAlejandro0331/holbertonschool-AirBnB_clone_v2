e450640ee5a77f073595605e52dc805feb33e1ea
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-13 16:02:34 -0500

    adding task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive
+    from the contents of the web_static"""
+from os import mkdir
+from fabric.api import local, run, put
+from os.path import isdir
+import time
+
+
+def do_pack():
+    if isdir('versions') is False:
+        local("mkdir versions")
+    file = "web_static_" + time.strftime("%Y%m%d%H%M%S", time.gmtime())\
+        + ".tgz"
+    result = local("tar -cvzf versions/{} web_static ".format(file))
+    if result.failed:
+        return None
+    return result
